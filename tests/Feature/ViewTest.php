b3f3e9ee30e0bd1c9242b5ac572c6ba2eea1acb0
Philipp Daun <post@philippdaun.net>
2024-03-09 16:11:47 +0100

    Test view data

--- tests/Feature/ViewTest.php
+++ tests/Feature/ViewTest.php
@@ -6,3 +6,16 @@ test('renders views', function () {
 
     $this->view('welcome')->assertSee('Welcome to Laravel Latte');
 });
+
+test('passes data into view', function () {
+    $app = $this->createApplication();
+    $this->createAndBootServiceProvider($app);
+
+    $this->view('basic-data', [
+        'name' => 'John',
+        'age' => 31,
+        'hobbies' => ['fishing', 'hiking', 'camping'],
+    ])->assertSee('My name is John')
+        ->assertSee('I am 31 years old')
+        ->assertSee('I enjoy fishing, hiking, camping');
+});
