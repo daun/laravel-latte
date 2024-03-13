c45e02dd2385bb703fe6681da8a0dd53ecf3ac2a
Philipp Daun <post@philippdaun.net>
2024-03-13 14:37:02 +0100

    Test filters added via facade

--- tests/Feature/FacadeTest.php
+++ tests/Feature/FacadeTest.php
@@ -1,10 +1,19 @@
 <?php
 
 use Daun\LaravelLatte\Facades\Latte;
+use Illuminate\Support\Str;
 use Latte\Engine;
 
 test('creates correct facade instance', function () {
     $this->bootServiceProvider();
 
     expect(Latte::getFacadeRoot())->toBeInstanceOf(Engine::class);
+});
+
+test('provides filters from facade', function () {
+    $this->bootServiceProvider();
+
+    Latte::addFilter('plural', fn($str) => Str::plural($str));
+
+    $this->latte('{$word|plural}', ['word' => 'car'])->assertSee('cars');
 });
